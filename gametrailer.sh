a7febf8742abfa5db03566ad0a6a5a8a5d3baca5
Hai Vu <haihovu@gmail.com>
2017-03-10 21:07:17 -0500

    Updated build scripts

--- gametrailer.sh
+++ gametrailer.sh
@@ -1,1 +1,1 @@
-java -cp antdist/midas.jar:antdist/gametrailer.jar:antdist/dejavu_game.jar:antdist/silk_icons.jar -Dsun.java2d.opengl=true trailer.GameTrailer
+java -cp antdist/dejavu.jar:antdist/gametrailer.jar:antdist/dejavu_game.jar:antdist/silk_icons.jar -Dsun.java2d.opengl=true trailer.GameTrailer
