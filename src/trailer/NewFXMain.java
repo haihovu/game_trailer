ba515858afc53b49e3188f500d422f1bbbef9268
Hai Vu <haihovu@gmail.com>
2017-03-22 20:00:26 -0400

    Minor tweaks

--- src/trailer/NewFXMain.java
+++ src/trailer/NewFXMain.java
@@ -7,7 +7,6 @@ package trailer;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -23,12 +22,8 @@ public class NewFXMain extends Application {
 	public void start(Stage primaryStage) {
 		Button btn = new Button();
 		btn.setText("Say 'Hello World'");
-		btn.setOnAction(new EventHandler<ActionEvent>() {
-			
-			@Override
-			public void handle(ActionEvent event) {
-				System.out.println("Hello World!");
-			}
+		btn.setOnAction((ActionEvent event) -> {
+			System.out.println("Hello World!");
 		});
 		
 		StackPane root = new StackPane();
